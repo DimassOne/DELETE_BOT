41934251d23f777ea4c95073d899647a24ae19f4
Dmitrii Rogonian <dimarru1@rambler.ru>
2020-04-18 14:27:45 +0300

    Second commit

--- main.py
+++ main.py
@@ -11,7 +11,28 @@ def messag(message):
     btn2 = types.KeyboardButton('Лемтыбож')
     btn3 = types.KeyboardButton('Лемты')
     markup.add(btn1, btn2, btn3)
-    bot.send_message(chat_id, "Здравствуй другc!", reply_markup=markup)
+    bot.send_message(chat_id, "Здравствуй друг! Из какого ты города?", reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def next_city(message):
+    chat_id = message.chat.id
+    markup = types.ReplyKeyboardRemove()
+    if message.text == 'Лемтыбож':
+        send = bot.send_message(chat_id, "Введите ваш номер телефона:", reply_markup=markup)
+        bot.register_next_step_handler(send, next_phone)
+
+def next_phone(message):
+    try:
+        chat_id = message.chat.id
+        int(message.text)
+        send = bot.send_message(chat_id, "Введите ваше имя: ")
+        #bot.register_next_step_handler(send, next_)
+    except Exception as err:
+        msg = bot.send_message(chat_id, "Вы что-то ввели не так...")
+        bot.register_next_step_handler(msg, next_phone)
+
+
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
